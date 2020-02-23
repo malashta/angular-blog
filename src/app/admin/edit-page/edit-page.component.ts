060fa650f51ccf80b02af1b821d2a0ca02d89196
Tallerr <malashta@gmail.com>
2020-02-23 21:23:10 +0700

    add edit post form

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {PostService} from '../../shared/post.service';
+import {switchMap} from 'rxjs/operators';
+import {Post} from '../../shared/interfaces';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  editForm: FormGroup;
+  pSub;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) { }
 
   ngOnInit() {
+    this.initForm();
+    this.pSub = this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.postService.getById(params['id']);
+      }))
+      .subscribe((post: Post) => {
+        this.editForm.setValue({
+          title: post.title,
+          text: post.text
+        });
+      });
+  }
+
+  ngOnDestroy() {
+    this.pSub.unsubscribe();
+  }
+
+  initForm() {
+    this.editForm = new FormGroup({
+      title: new FormControl('', Validators.required),
+      text: new FormControl('', Validators.required),
+    });
   }
 
 }
